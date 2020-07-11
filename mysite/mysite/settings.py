08f590ad26ec45235ce1ccf61f2401c60b759021
medetzhan1996 <67154220+medetzhan1996@users.noreply.github.com>
2020-07-12 01:35:12 +0600

    s

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'site-crm',
-        'USER': 'postgres',
-        'PASSWORD': '1q2w3e',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
